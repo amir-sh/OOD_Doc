4bd347634dd6c7e3c221d7ee46890e857449b83a
alireza70 <rezaei70@gmail.com>
2014-03-02 21:58:08 +0330

    alaki

---
+++ WindowsFormsApplication1/Model/Util/LabelThing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WindowsFormsApplication1.Model.AssetManagement;
+using WindowsFormsApplication1.Model.LabelManagement;
+
+namespace WindowsFormsApplication1.Model.Util
+{
+    class LabelThing : Thing
+    {
+        [Key]
+        public int id {get; set;}
+        public virtual LabelInstance assingnedlabel ;
+    }
+}
