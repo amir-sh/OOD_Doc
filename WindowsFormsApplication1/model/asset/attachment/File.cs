e3a3deb1d0e9182249efaec8dfc37755af3ce10e
amir-sh <sharifadeh.amir@gmail.com>
2014-03-02 21:55:05 +0330

    orm2

---
+++ WindowsFormsApplication1/model/asset/attachment/File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.model.asset.attachment
+{
+    class File
+    {
+        [Key]
+        public int id { get; set; }
+        public string attribute1 { get; set; }
+        public string attribute2 { get; set; }
+    }
+}
---
+++ WindowsFormsApplication1/model/label/LabelManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.model.label
+{
+    class LabelManager
+    {
+        [Key]
+        public int id { get; set; }
+        public ICollection<Label> labels { get; set; }
+        public string attribute { get; set; }
+    }
+}
